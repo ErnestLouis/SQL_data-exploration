c046193023a40680a2fe6732d3cc2069f6f6b341
Ernest <ernest_louis@outlook.com>
2022-03-13 19:52:43 -0400

    SELECT Total CASES VS TOTAL DEATHS PER COUNTRY

--- SQLQuery_ExplorationProject.sql
+++ SQLQuery_ExplorationProject.sql
@@ -12,3 +12,8 @@ SELECT Location, date, total_cases, new_cases, total_deaths, population
 FROM Covid19Project..Covid_deaths
 ORDER BY 1,2 --ordered by location and date
 
+--SELECT Total CASES VS TOTAL DEATHS PER COUNTRY
+
+SELECT Location, date, total_cases,total_deaths,(total_deaths/total_cases)*100 as death_percentage--to get percentage
+FROM Covid19Project..Covid_deaths
+ORDER BY 1,2 --ordered by location and date
