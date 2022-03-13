523b64501e8b8ab78328abe6da1232986350ad3b
Ernest <ernest_louis@outlook.com>
2022-03-13 19:48:10 -0400

    SELECT DATA THE WILL BE USED

--- SQLQuery_ExplorationProject.sql
+++ SQLQuery_ExplorationProject.sql
@@ -6,3 +6,9 @@ ORDER BY 3,4
 SELECT * FROM Covid19Project..Covid_vaccinations
 ORDER BY 3,4
 
+--SELECT DATA THE WILL BE USED
+
+SELECT Location, date, total_cases, new_cases, total_deaths, population
+FROM Covid19Project..Covid_deaths
+ORDER BY 1,2 --ordered by location and date
+
