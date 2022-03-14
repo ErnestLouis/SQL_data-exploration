5d32e12eb82670b266d4189eb01f9cd675a40bf7
Ernest <ernest_louis@outlook.com>
2022-03-13 20:33:01 -0400

    SELECT TOTAL CASES VS POPULATION

--- SQLQuery_ExplorationProject.sql
+++ SQLQuery_ExplorationProject.sql
@@ -19,7 +19,16 @@ FROM Covid19Project..Covid_deaths
 ORDER BY 1,2 --ordered by location and date
 
 --SELECT Total CASES VS TOTAL DEATHS IN THE UNITED STATES
+--Projects liklihood of death from countracting covid in designated country
 SELECT Location, date, total_cases,total_deaths,ROUND((total_deaths/total_cases)*100,2) as death_percentage--to get percentage rounded 2 decimal places
 FROM Covid19Project..Covid_deaths
 WHERE location = 'United States'
 ORDER BY 1,2 --ordered by location and date
+
+--SELECT TOTAL CASES VS POPULATION
+SELECT Location, date, population,total_cases,ROUND((total_cases/population)*100,3) as case_percentage--to get percentage rounded 3 decimal places
+FROM Covid19Project..Covid_deaths
+WHERE location like '%states%'
+ORDER BY 1,2 
+
+
