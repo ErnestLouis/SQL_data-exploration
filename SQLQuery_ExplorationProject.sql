3acf0747c0b2011355c9164d4de7fe81125ba7b4
Ernest <ernest_louis@outlook.com>
2022-03-13 21:34:43 -0400

    DISPLAY CONTINENT W/HIGHEST DEATH COUNT PER POPULATION

--- SQLQuery_ExplorationProject.sql
+++ SQLQuery_ExplorationProject.sql
@@ -1,21 +1,30 @@
+/*
+Covid 19 Data Exploration 
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+*/
+
 --View imported Data from both tables
 
 SELECT * FROM Covid19Project..Covid_deaths
+WHERE continent is not null 
 ORDER BY 3,4
 
 SELECT * FROM Covid19Project..Covid_vaccinations
+WHERE continent is not null 
 ORDER BY 3,4
 
 --SELECT DATA THE WILL BE USED
 
 SELECT Location, date, total_cases, new_cases, total_deaths, population
 FROM Covid19Project..Covid_deaths
+WHERE continent is not null 
 ORDER BY 1,2 --ordered by location and date
 
 --SELECT Total CASES VS TOTAL DEATHS PER COUNTRY
 
 SELECT Location, date, total_cases,total_deaths,(total_deaths/total_cases)*100 as death_percentage--to get percentage
 FROM Covid19Project..Covid_deaths
+WHERE continent is not null 
 ORDER BY 1,2 --ordered by location and date
 
 --SELECT Total CASES VS TOTAL DEATHS IN THE UNITED STATES
@@ -25,7 +34,7 @@ FROM Covid19Project..Covid_deaths
 WHERE location = 'United States'
 ORDER BY 1,2 --ordered by location and date
 
---SELECT TOTAL CASES VS POPULATION
+--SELECT TOTAL CASES VS POPULATION IN THE UNITED STATES
 SELECT Location, date, population,total_cases,ROUND((total_cases/population)*100,3) as case_percentage--to get percentage rounded 3 decimal places
 FROM Covid19Project..Covid_deaths
 WHERE location like '%states%'
@@ -42,5 +51,13 @@ ORDER BY case_percentage desc
 SELECT Location, MAX(CAST(total_deaths as bigint)) AS highest_death_count
 FROM Covid19Project..Covid_deaths
 WHERE continent IS NOT NULL
-Group BY Location, Population
+Group BY Location
 ORDER BY highest_death_count desc
+
+
+--DISPLAY CONTINENT W/HIGHEST DEATH COUNT PER POPULATION
+SELECT continent, MAX(CAST(total_deaths as bigint)) AS highest_death_count
+FROM Covid19Project..Covid_deaths
+WHERE continent  IS NOT NULL
+GROUP BY continent
+ORDER BY  highest_death_count desc
